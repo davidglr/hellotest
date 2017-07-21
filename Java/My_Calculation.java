9e4cefd4d65bd306e17bbdd3f11d66b8477aa2a9
davidglr <davidmine76@gmail.com>
2017-07-21 14:10:34 -0400

    This is javacheck

--- Java/My_Calculation.java
+++ Java/My_Calculation.java
@@ -1,7 +1,7 @@
 public class My_Calculation extends Calculation {
    public void multiplication(int x, int y) {
       z = x * y;
-      System.out.println"The product of the given numbers:"+z);
+      System.out.println("The product of the given numbers:"+z);
    }
 	
    public static void main(String args[]) {
